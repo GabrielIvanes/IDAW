81e922eab7415463c3060f3b42dff9001e8588e6
Gabriel Ivanes <ivanes.gabriel@gmail.com>
2023-10-11 08:18:26 +0200

    correction de l'accueil

--- TP2-PHP/SitePro/v3/accueil.php
+++ TP2-PHP/SitePro/v3/accueil.php
@@ -7,7 +7,7 @@
     skills by taking new courses online and develop a few projects by my
     own.
 </div>' : '<div class="contenu">
-Bonjour ! Je suis Gabriel Ivanes et je suis actuellement dans ma deuxième année d\école d\'ingénieur à l\'IMT Nord Europe dans le nord de la France.
+Bonjour ! Je suis Gabriel Ivanes et je suis actuellement dans ma deuxième année d\'école d\'ingénieur à l\'IMT Nord Europe dans le nord de la France.
 J\'ai toujours été passionné par l\'informatique. Mes intérêts pour le développement web ont commencé en septembre 2022 quand j\'ai pris un cours en ligne
 sur OpenClassrooms. Depuis, je continue de développer mes compétences en prenant part à de nouveaux cours en ligne et en développant quelques projets par moi-même.
 </div>' ?>
