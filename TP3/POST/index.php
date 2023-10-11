59359561a1db9c46913354601ecf0d22326fe170
Gabriel Ivanes <ivanes.gabriel@gmail.com>
2023-10-11 22:36:40 +0200

    Correction des cookies

--- TP3/POST/index.php
+++ TP3/POST/index.php
@@ -1,15 +1,7 @@
-<form id="style_form" action="index.php" method="GET">
+<form id="style_form" action="login.php" method="GET">
     <select name="css">
         <option value="style1">style1</option>
         <option value="style2">style2</option>
     </select>
     <input type="submit" value="Appliquer" />
 </form>
-
-<?php 
-
-if (isset($_GET['css'])) {
-    setcookie('style', $_GET['css']);
-}
-
-?>
--- TP3/POST/login.php
+++ TP3/POST/login.php
@@ -1,38 +1,52 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php echo '<link href="'.(isset($_COOKIE['style']) ? $_COOKIE['style'] : 'style1').'.css" rel="stylesheet" />' ?>
-    <title>Formulaire</title>
-</head>
-
-
-
-<body>
-    <?php 
+ <?php 
     session_start();
     session_unset();
-session_destroy(); 
+    session_destroy(); 
+    if (isset($_GET['css'])) {
+        setcookie('style', $_GET['css']);
+    }
+    if ($_GET['css'] === 'style1') {
+        $css = '<link rel="stylesheet" href="style1.css">';
+    } else {
+        $css = '<link rel="stylesheet" href="style2.css">';
+}
 ?>
 
-    <form id="login_form" action="connected.php" method="POST">
-        <table>
-            <tr>
-                <th>Login :</th>
-                <td><input type="text" name="login"></td>
-            </tr>
-            <tr>
-                <th>Mot de passe :</th>
-                <td><input type="password" name="password"></td>
-            </tr>
-            <tr>
-                <th></th>
-                <td><input type="submit" value="Se connecter..." /></td>
-            </tr>
-        </table>
-    </form>
-</body>
+ <!DOCTYPE html>
+ <html lang="fr">
 
-</html>
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <?php echo $css; ?>
+     <title>Formulaire</title>
+ </head>
+
+
+
+ <body>
+
+
+     <form id="login_form" action="connected.php" method="POST">
+         <table>
+             <tr>
+                 <th>Login :</th>
+                 <td><input type="text" name="login"></td>
+             </tr>
+             <tr>
+                 <th>Mot de passe :</th>
+                 <td><input type="password" name="password"></td>
+             </tr>
+             <tr>
+                 <th></th>
+                 <td><input type="submit" value="Se connecter..." /></td>
+             </tr>
+         </table>
+     </form>
+
+     <form action="index.php" method="POST">
+         <input type="submit" value="Changer le style">
+     </form>
+ </body>
+
+ </html>
