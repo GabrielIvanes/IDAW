038cc4936c157633d16dd6b137fddb9a64b1ea61
Gabriel Ivanes <ivanes.gabriel@gmail.com>
2023-10-17 11:39:50 +0200

    factorisation et réparation de bug

--- TP4/exo3/init_db.php
+++ TP4/exo3/init_db.php
@@ -1,6 +1,6 @@
 <?php
-require_once('../config.php');
 
+require(__DIR__ . '/../config.php');
 $connectionString = "mysql:host=". _MYSQL_HOST;
 if(defined('_MYSQL_PORT'))
     $connectionString .= ";port=". _MYSQL_PORT;
@@ -11,7 +11,7 @@ try {
     $pdo = new PDO($connectionString, _MYSQL_USER, _MYSQL_PASSWORD, $options);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sqlFile = file_get_contents('./sql/create_db.sql');
+    $sqlFile = file_get_contents(__DIR__.'/sql/create_db.sql');
     $pdo -> exec($sqlFile);
     
     echo 'Connexion réussie';
--- TP4/users.php
+++ TP4/users.php
@@ -12,8 +12,9 @@
 
 <body>
     <?php
-    require_once('./exo3/init_db.php');
-    require_once('config.php');
+
+    // require_once('./exo3/init_db.php');
+    require_once('./config.php');
 
     $isUpdatingUser = false;
     $updatingId = -1;
@@ -29,20 +30,7 @@
        
     }
 
-    $connectionString = "mysql:host=". _MYSQL_HOST;
-    if(defined('_MYSQL_PORT'))
-        $connectionString .= ";port=". _MYSQL_PORT;
-    $connectionString .= ";dbname=" . _MYSQL_DBNAME;
-    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' );
-    $pdo = NULL;
-    try {
-        $pdo = new PDO($connectionString,_MYSQL_USER,_MYSQL_PASSWORD,$options);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $erreur) {
-        echo 'Erreur : '.$erreur->getMessage();
-    }
-
+    require_once(__DIR__.'/connectDb.php');
 
     $request = $pdo->prepare("select * from users");
     $request->execute();
